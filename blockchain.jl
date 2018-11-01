b0e554d9c7487a97874d1e274f520f5dd88da586
HuLaTown <huzhiwei@outlook.com>
2018-11-01 20:32:59 +0800

    step2-block

--- blockchain.jl
+++ blockchain.jl
@@ -1,15 +1,32 @@
+struct Transaction
+    sender::String
+    recipient::String
+    amount::Float64
+end
+
+mutable struct Block
+    index::Int64
+    timestamp::Int64
+    transaction_list::Array(Transaction)
+    proof::String
+    previous_hash::String
+end
+
+
 mutable struct Blockchain
     chain
     current_transaction
 end
 
 function init(blockchain::Blockchain)
+    blockchain.chain = []
 end
 
 function new_block(;blockchain::Blockchain, proof, previous_hash = nothing)
 end
 
-function new_transaction()
+function new_transaction(;blockchain::Blockchain, sender, recipient, amount)
+
 end
 
 function blockhash()
