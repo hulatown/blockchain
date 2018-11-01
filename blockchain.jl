9ff4e4655d4fa432839a9a7360aebf845d6a6db1
HuLaTown <huzhiwei@outlook.com>
2018-11-01 22:35:43 +0800

    step3: new transaction

--- blockchain.jl
+++ blockchain.jl
@@ -7,26 +7,28 @@ end
 mutable struct Block
     index::Int64
     timestamp::Int64
-    transaction_list::Array(Transaction)
+    transaction_list::Array{Transaction}
     proof::String
     previous_hash::String
 end
 
 
 mutable struct Blockchain
-    chain
-    current_transaction
+    chain::Array{Block}
+    current_transaction::Array{Transaction}
 end
 
 function init(blockchain::Blockchain)
     blockchain.chain = []
+    blockchain.current_transaciton = []
 end
 
 function new_block(;blockchain::Blockchain, proof, previous_hash = nothing)
 end
 
 function new_transaction(;blockchain::Blockchain, sender, recipient, amount)
-
+    new_tx = Transaction(sender, recipient, amount)
+    push!(blockchain.chain, new_tx)
 end
 
 function blockhash()
